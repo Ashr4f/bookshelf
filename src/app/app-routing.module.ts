d7362694273df56ff4fc43de1233916ff831cbc9
Ashraf <ashraf@post.com>
2019-10-06 17:18:15 +0200

    Redirect to homepage on 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  { path: "register", component: RegisterComponent },
+  {
+    path: "**",
+    redirectTo: ""
+  }
 ];
 
 @NgModule({
