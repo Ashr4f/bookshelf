e92f1e72c40b7c297df781968c36e0ab3aee4fb0
Ashraf <ashraf@post.com>
2019-10-10 00:20:00 +0200

    Added ng-zorro form to edit-book component

--- src/app/create-book/create-book.component.ts
+++ src/app/create-book/create-book.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Apollo } from "apollo-angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ADD_BOOK_MUTATION } from "../graphql";
-import * as RandExp from "randexp";
 import gql from "graphql-tag";
 
 type Response = {
@@ -15,7 +15,11 @@ type Response = {
   styleUrls: ["./create-book.component.scss"]
 })
 export class CreateBookComponent implements OnInit {
-  isbn: string = new RandExp(/978-\d-\d{3}-\d{5}-\d/).gen();
+  validateForm: FormGroup;
+  bookExists: Boolean = false;
+  CreateBookErrorMessage: string;
+
+  isbn: string = "";
   title: string = "";
   author: string = "";
   editor: string = "";
@@ -26,9 +30,38 @@ export class CreateBookComponent implements OnInit {
   format: string = "";
   BookFormats: [] = [];
 
-  constructor(public apollo: Apollo) {}
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+    console.log(this.validateForm.status);
+
+    if (this.validateForm.status === "VALID") {
+      this.isbn = this.validateForm.value.bookISBN;
+      this.title = this.validateForm.value.title;
+      this.author = this.validateForm.value.author;
+      this.lang = this.validateForm.value.bookLanguage;
+      this.cover = this.validateForm.value.cover;
+      this.format = this.validateForm.value.bookFormat;
+      this.schools = this.validateForm.value.bookSchool;
+      this.createBook();
+    }
+  }
+
+  constructor(public apollo: Apollo, private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.validateForm = this.fb.group({
+      bookISBN: [null, [Validators.required]],
+      title: [null, [Validators.required]],
+      author: [null, [Validators.required]],
+      bookFormat: [null, [Validators.required]],
+      bookSchool: [null, [Validators.required]],
+      bookLanguage: [null, [Validators.required]],
+      cover: [null, [Validators.required]]
+    });
+
     this.apollo
       .watchQuery<Response>({
         query: gql`
@@ -68,8 +101,15 @@ export class CreateBookComponent implements OnInit {
           format: this.format
         }
       })
-      .subscribe(response => {
-        console.log(response);
-      });
+      .subscribe(
+        response => {
+          console.log(response);
+        },
+        err => {
+          this.bookExists = true;
+          this.CreateBookErrorMessage =
+            "Another book already exists with the same key!";
+        }
+      );
   }
 }
--- src/app/edit-book/edit-book.component.ts
+++ src/app/edit-book/edit-book.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Apollo } from "apollo-angular";
 import { BOOK_QUERY, EDIT_BOOK_MUTATION } from "../graphql";
-import { ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Subscription } from "apollo-client/util/Observable";
 import gql from "graphql-tag";
 
@@ -16,6 +17,10 @@ type Response = {
   styleUrls: ["./edit-book.component.scss"]
 })
 export class EditBookComponent implements OnInit {
+  validateForm: FormGroup;
+  editHasErrors: boolean = false;
+  EditBookErrorMessage: string;
+
   isbn: string = "";
   title: string = "";
   author: string = "";
@@ -29,10 +34,46 @@ export class EditBookComponent implements OnInit {
   BookFormats: [] = [];
   bookObject: object = {};
 
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+      if (this.validateForm.controls[i].status === "INVALID") {
+        this.editHasErrors = true;
+        this.EditBookErrorMessage = "Please fill in the blank(s)";
+      }
+    }
+
+    if (this.validateForm.status === "VALID") {
+      this.title = this.validateForm.value.title;
+      this.author = this.validateForm.value.author;
+      this.lang = this.validateForm.value.bookLanguage;
+      this.cover = this.validateForm.value.cover;
+      this.format = this.validateForm.value.bookFormat;
+      this.schools = this.validateForm.value.bookSchool;
+      this.editHasErrors = false;
+      this.EditBookErrorMessage = "";
+      this.editCurrentBook();
+    }
+  }
+
   private routeSub: Subscription;
-  constructor(public apollo: Apollo, private route: ActivatedRoute) {}
+  constructor(
+    public apollo: Apollo,
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.validateForm = this.fb.group({
+      title: [null, [Validators.required]],
+      author: [null, [Validators.required]],
+      bookFormat: [null, [Validators.required]],
+      bookSchool: [null, [Validators.required]],
+      bookLanguage: [null, [Validators.required]],
+      cover: [null, [Validators.required]]
+    });
     this.routeSub = this.route.params.subscribe(params => {
       this.isbn = params["id"];
     });
@@ -76,6 +117,15 @@ export class EditBookComponent implements OnInit {
         this.schools = response.data.book.availabilities;
         this.format = response.data.book.format;
         this.currentSchool = response.data.book.availabilities[0].school.slug;
+
+        this.validateForm = this.fb.group({
+          title: [this.title, [Validators.required]],
+          author: [this.author, [Validators.required]],
+          bookFormat: [this.format, [Validators.required]],
+          bookSchool: [this.currentSchool, [Validators.required]],
+          bookLanguage: [this.lang, [Validators.required]],
+          cover: [this.cover, [Validators.required]]
+        });
       });
   }
 
@@ -98,8 +148,13 @@ export class EditBookComponent implements OnInit {
           format: this.format
         }
       })
-      .subscribe(response => {
-        console.log(response);
-      });
+      .subscribe(
+        response => {
+          this.router.navigate([`/books/${this.isbn}`]);
+        },
+        err => {
+          console.log(err);
+        }
+      );
   }
 }
