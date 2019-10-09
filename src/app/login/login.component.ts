7d090ee378de48bb364c0444ac630903dca6be04
Ashraf <ashraf@post.com>
2019-10-10 00:20:40 +0200

    Changed login form (angular)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,27 +1,71 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Apollo } from "apollo-angular";
+import { LOGIN_MUTATION } from "../graphql";
+import { ActivatedRoute, Router } from "@angular/router";
+
+type LoginData = {
+  loginWithBasic: { token: string; connected: Boolean };
+};
 
 @Component({
-  selector: "nz-demo-form-horizontal-login",
+  selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
   validateForm: FormGroup;
+  user: string = "";
+  password: string = "";
+  wrongData: Boolean = false;
 
   submitForm(): void {
     for (const i in this.validateForm.controls) {
       this.validateForm.controls[i].markAsDirty();
       this.validateForm.controls[i].updateValueAndValidity();
     }
+    if (this.validateForm.status === "VALID") {
+      this.user = this.validateForm.value.userName;
+      this.password = this.validateForm.value.password;
+      this.login();
+    }
   }
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public apollo: Apollo,
+    private fb: FormBuilder
+  ) {}
+
+  login() {
+    this.apollo
+      .mutate<LoginData>({
+        mutation: LOGIN_MUTATION,
+        variables: {
+          login: this.user,
+          pass: this.password,
+          useCookie: false
+        }
+      })
+      .subscribe(
+        response => {
+          this.wrongData = false;
+          localStorage.setItem("token", response.data.loginWithBasic.token);
+          if (response.data.loginWithBasic.connected) {
+            this.router.navigate(["/"]);
+          }
+        },
+        err => {
+          this.wrongData = true;
+          console.log(err);
+        }
+      );
+  }
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
       userName: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-      remember: [true]
+      password: [null, [Validators.required]]
     });
   }
 }
