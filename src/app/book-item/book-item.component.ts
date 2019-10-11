2e3bfee9c351b6b279c14d5ed66d991c1a75cd85
Ashraf <ashraf@post.com>
2019-10-11 13:48:34 +0200

    Working on book-item (angular)

--- src/app/book-item/book-item.component.ts
+++ src/app/book-item/book-item.component.ts
@@ -16,6 +16,11 @@ export class BookItemComponent implements OnInit {
   tooltips = ["terrible", "bad", "normal", "good", "wonderful"];
   bookReview: number;
   myBookReview: number;
+  bookBorrowerUID: string;
+  bookBorrowerSlug: string;
+  bookBorrowerName: string;
+  BookAvailability: boolean = true;
+  BookCurrentSchoolSlug: string;
   user: any;
   loading: boolean = true;
   reviewUID: string;
@@ -38,15 +43,30 @@ export class BookItemComponent implements OnInit {
       this.gqlQueries.getBookByISBN(this.isbn).then((bookData: any) => {
         this.user = userData.data.consumer;
         this.bookData = bookData.data.book;
+
+        console.log(this.bookData);
         this.loading = bookData.loading;
+        this.BookAvailability = bookData.data.book.availabilities[0].available;
+
+        if (bookData.data.book.availabilities[0].borrower) {
+          this.bookBorrowerUID =
+            bookData.data.book.availabilities[0].borrower.uid;
+          this.bookBorrowerSlug =
+            bookData.data.book.availabilities[0].borrower.slug;
+          this.bookBorrowerName =
+            bookData.data.book.availabilities[0].borrower.name;
+        }
 
         if (this.bookData.reviews.nodes.length > 0) {
-          this.reviewUID = this.bookData.reviews.nodes[0].uid;
-          if (
-            this.user.owner.uid === this.bookData.reviews.nodes[0].reviewer.uid
-          ) {
-            this.myBookReview = this.bookData.reviews.nodes[0].note;
-            this.bookReview = this.myBookReview;
+          for (let i = 0; i < this.bookData.reviews.nodes.length; i++) {
+            if (
+              this.user.owner.uid ===
+              this.bookData.reviews.nodes[i].reviewer.uid
+            ) {
+              this.reviewUID = this.bookData.reviews.nodes[i].uid;
+              this.myBookReview = this.bookData.reviews.nodes[i].note;
+              this.bookReview = this.myBookReview;
+            }
           }
         }
       });
@@ -55,6 +75,28 @@ export class BookItemComponent implements OnInit {
 
   ngOnDestroy() {
     this.routeSub.unsubscribe();
+  }
+
+  rentCurrentBook() {
+    this.gqlQueries
+      .rentBook(this.isbn, this.bookData.availabilities[0].school.slug)
+      .then((res: any) => {
+        console.log(res);
+        this.BookAvailability = res.data.borrowBook.availabilities[0].available;
+        this.bookBorrowerUID =
+          res.data.borrowBook.availabilities[0].borrower.uid;
+      });
+  }
+
+  returnCurrentBook() {
+    this.gqlQueries
+      .returnBook(this.isbn, this.bookData.availabilities[0].school.slug)
+      .then((res: any) => {
+        console.log(res);
+        this.BookAvailability = res.data.borrowBook.availabilities[0].available;
+        this.bookBorrowerUID =
+          res.data.borrowBook.availabilities[0].borrower.uid;
+      });
   }
 
   sendBookReview() {
@@ -68,15 +110,24 @@ export class BookItemComponent implements OnInit {
           ""
         )
         .then((res: any) => {
-          console.log(res);
+          this.myBookReview = this.bookReview;
         })
         .catch((err: any) => {
-          console.log(err);
+          console.error(err);
         });
     }
 
     if (this.bookReview === 0) {
       console.log("delete");
+
+      this.gqlQueries
+        .deleteBookReview(this.reviewUID)
+        .then((res: any) => {
+          this.myBookReview = undefined;
+        })
+        .catch((err: any) => {
+          console.error(err);
+        });
     }
 
     if (this.myBookReview === undefined) {
@@ -90,10 +141,11 @@ export class BookItemComponent implements OnInit {
           ""
         )
         .then((res: any) => {
-          console.log(res);
+          this.myBookReview = this.bookReview;
+          this.reviewUID = res.data.addBookReview.uid;
         })
         .catch((err: any) => {
-          console.log(err);
+          console.error(err);
         });
     }
   }
