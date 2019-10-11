bf4f59eea68332388e7058b7e3c13fa950df0d67
Ashraf <ashraf@post.com>
2019-10-11 13:49:06 +0200

    Working on book-list (angular)

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -8,10 +8,12 @@ import { QueriesService } from "src/app/services/gql-queries.service";
   styleUrls: ["./book-list.component.scss"]
 })
 export class BookListComponent implements OnInit {
-  allBooks: Book[] = [];
+  allBooks: any[] = [];
   loading: boolean = true;
   tooltips = ["terrible", "bad", "normal", "good", "wonderful"];
-  bookReview: number;
+  booksReviews: number[] = [];
+  reviewersTotalCount: number;
+  allNotes: any[] = [];
 
   constructor(private gqlQueries: QueriesService) {}
 
@@ -19,7 +21,26 @@ export class BookListComponent implements OnInit {
     this.gqlQueries.getAllBooks().then((booksData: any) => {
       this.loading = booksData.loading;
       this.allBooks = booksData.data.books.nodes;
-      console.log(booksData);
+      console.log(booksData.data.books.nodes);
+
+      for (let i = 0; i < this.allBooks.length; i++) {
+        this.allNotes[i] = this.allBooks[i].reviews.nodes;
+      }
+
+      for (let i = 0; i < this.allNotes.length; i++) {
+        if (this.allNotes[i].length > 0) {
+          let somme = 0;
+          this.allBooks[i].reviews.nodes.map((a: any) => {
+            somme += a.note;
+          });
+
+          this.booksReviews.push(
+            Math.round((somme / this.allBooks[i].reviews.totalCount) * 10) / 10
+          );
+        } else {
+          this.booksReviews.push(0);
+        }
+      }
     });
   }
 }
