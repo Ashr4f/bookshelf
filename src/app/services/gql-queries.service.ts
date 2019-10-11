091cc3566d27aea06cf09c86a2853577c5dfe7b5
Ashraf <ashraf@post.com>
2019-10-11 13:51:04 +0200

    Added common gql-queries service

--- src/app/services/gql-queries.service.ts
+++ src/app/services/gql-queries.service.ts
@@ -5,7 +5,13 @@ import {
   BOOK_QUERY,
   ADD_BOOK_REVIEW,
   ALL_BOOKS_QUERY,
-  EDIT_BOOK_REVIEW
+  EDIT_BOOK_REVIEW,
+  DELETE_BOOK_REVIEW,
+  EDIT_BOOK_MUTATION,
+  BOOK_SELECT_OPTIONS,
+  ADD_BOOK_MUTATION,
+  RENT_BOOK,
+  RETURN_BOOK
 } from "../graphql";
 
 @Injectable({
@@ -45,8 +51,94 @@ export class QueriesService {
       .toPromise();
   }
 
+  async getBookSelectOptions() {
+    return await this.apollo
+      .query({
+        query: BOOK_SELECT_OPTIONS
+      })
+      .toPromise();
+  }
+
+  async createBook(
+    isbn: any,
+    title: any,
+    author: any,
+    editor: any,
+    lang: any,
+    cover: any,
+    schools: any,
+    format: any
+  ) {
+    return await this.apollo
+      .mutate({
+        mutation: ADD_BOOK_MUTATION,
+        variables: {
+          isbn: isbn,
+          title: title,
+          author: author,
+          editor: editor,
+          lang: lang,
+          cover: cover,
+          schools: schools,
+          format: format
+        }
+      })
+      .toPromise();
+  }
+
+  async editCurrentBook(
+    isbn: any,
+    title: any,
+    author: any,
+    editor: any,
+    lang: any,
+    cover: any,
+    schools: any,
+    format: any
+  ) {
+    return await this.apollo
+      .mutate({
+        mutation: EDIT_BOOK_MUTATION,
+        variables: {
+          isbn: isbn,
+          title: title,
+          author: author,
+          editor: editor,
+          lang: lang,
+          cover: cover,
+          schools: schools,
+          format: format
+        }
+      })
+      .toPromise();
+  }
+
+  async rentBook(book: string, school: string) {
+    return this.apollo
+      .mutate({
+        mutation: RENT_BOOK,
+        variables: {
+          book: book,
+          school: school
+        }
+      })
+      .toPromise();
+  }
+
+  async returnBook(book: string, school: string) {
+    return this.apollo
+      .mutate({
+        mutation: RETURN_BOOK,
+        variables: {
+          book: book,
+          school: school
+        }
+      })
+      .toPromise();
+  }
+
   async addBookReview(isbn: any, note: any, lang: any, comment: any) {
-    this.apollo
+    return await this.apollo
       .mutate({
         mutation: ADD_BOOK_REVIEW,
         variables: {
@@ -62,7 +154,7 @@ export class QueriesService {
   }
 
   async editBookReview(uid: any, note: any, lang: any, comment: any) {
-    this.apollo
+    return await this.apollo
       .mutate({
         mutation: EDIT_BOOK_REVIEW,
         variables: {
@@ -75,6 +167,16 @@ export class QueriesService {
         }
       })
       .toPromise();
-    console.log(uid, note, lang, comment);
+  }
+
+  async deleteBookReview(uid: any) {
+    return await this.apollo
+      .mutate({
+        mutation: DELETE_BOOK_REVIEW,
+        variables: {
+          uid: uid
+        }
+      })
+      .toPromise();
   }
 }
